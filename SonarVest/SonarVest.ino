956595d23ed8d0401f922140ecd8c919da9e868e
TylerMurray1 <34780822+TylerMurray1@users.noreply.github.com>
2019-03-17 20:42:29 -0400

    Added analog motor portions

--- SonarVest/SonarVest.ino
+++ SonarVest/SonarVest.ino
@@ -11,6 +11,12 @@ const int trigPin5 = 24;
 const int echoPin5 = 25;
 const int trigPin6 = 22;
 const int echoPin6 = 23;
+const int motorPin1 = 13;
+const int motorPin2 = 12;
+const int motorPin3 = 11;
+const int motorPin4 = 10;
+const int motorPin5 = 9;
+const int motorPin6 = 8;
 
 // defines variables
 long duration1;
@@ -40,12 +46,35 @@ void setup() {
   pinMode(echoPin5, INPUT);
   pinMode(trigPin6, OUTPUT);
   pinMode(echoPin6, INPUT);
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+  pinMode(motorPin5, OUTPUT);
+  pinMode(motorPin6, OUTPUT);
   Serial.begin(9600); // Starts the serial communication
 
 }
 
+void vibrateIntensity(long duration, const int motorPin) {
+  if(duration <= 5000 and duration >= 3000) {
+    analogWrite(motorPin, 60);
+  }
+  else if(duration <= 3000 and duration >= 2000){
+    analogWrite(motorPin, 120);
+  }
+  else if(duration <= 2000){
+    analogWrite(motorPin, 255);
+  } 
+  else if(duration >= 7000){
+    analogWrite(motorPin, 0);
+  }
+}
+
 void loop() {
-  
+ 
+  //////////////////////////////////SENSOR 1//////////////////////////////////////
+
   // Clears the trigPin
   digitalWrite(trigPin1, LOW);
   delayMicroseconds(2); 
@@ -60,12 +89,18 @@ void loop() {
   
   // Calculating the distance
   distance1 = duration1*0.034/2;
+
+  vibrateIntensity(duration1, motorPin1);
   
   // Prints the distance on the Serial Monitor
   Serial.print("Distance1: ");
   Serial.println(duration1);
   
   delay(1000);
+  
+  //////////////////////////////////END SENSOR 1//////////////////////////////////
+
+  //////////////////////////////////SENSOR 2//////////////////////////////////////
   
   // Clears the trigPin
   digitalWrite(trigPin2, LOW);
@@ -81,12 +116,18 @@ void loop() {
   
   // Calculating the distance
   distance2 = duration2*0.034/2;
+
+  vibrateIntensity(duration2, motorPin2);
   
   // Prints the distance on the Serial Monitor
   Serial.print("Distance2: ");
   Serial.println(duration2);
   
   delay(1000);
+
+  //////////////////////////////////END SENSOR 2//////////////////////////////////
+  
+  //////////////////////////////////SENSOR 3//////////////////////////////////////
   
   // Clears the trigPin
   digitalWrite(trigPin3, LOW);
@@ -102,12 +143,18 @@ void loop() {
   
   // Calculating the distance
   distance3 = duration3*0.034/2;
+
+  vibrateIntensity(duration3, motorPin3);
   
   // Prints the distance on the Serial Monitor
   Serial.print("Distance3: ");
   Serial.println(duration3);
 
   delay(1000);
+
+  //////////////////////////////////END SENSOR 3//////////////////////////////////  
+
+  //////////////////////////////////SENSOR 4//////////////////////////////////////
   
   // Clears the trigPin
   digitalWrite(trigPin4, LOW);
@@ -123,12 +170,18 @@ void loop() {
   
   // Calculating the distance
   distance4 = duration4*0.034/2;
+
+  vibrateIntensity(duration4, motorPin4);
   
   // Prints the distance on the Serial Monitor
   Serial.print("Distance4: ");
   Serial.println(duration4);
 
   delay(1000);
+
+  //////////////////////////////////END SENSOR 4//////////////////////////////////  
+
+  //////////////////////////////////SENSOR 5//////////////////////////////////////
   
   // Clears the trigPin
   digitalWrite(trigPin5, LOW);
@@ -144,13 +197,19 @@ void loop() {
   
   // Calculating the distance
   distance5 = duration5*0.034/2;
+
+  vibrateIntensity(duration5, motorPin5);
   
   // Prints the distance on the Serial Monitor
   Serial.print("Distance5: ");
   Serial.println(duration5);
 
   delay(1000);
-  
+
+  //////////////////////////////////END SENSOR 5//////////////////////////////////  
+
+  //////////////////////////////////SENSOR 6//////////////////////////////////////
+
    // Clears the trigPin
   digitalWrite(trigPin6, LOW);
   delayMicroseconds(2); 
@@ -165,11 +224,15 @@ void loop() {
   
   // Calculating the distance
   distance6 = duration6*0.034/2;
+
+  vibrateIntensity(duration6, motorPin6);
   
   // Prints the distance on the Serial Monitor
   Serial.print("Distance6: ");
   Serial.println(duration6);
 
   delay(1000);
+
+  //////////////////////////////////END SENSOR 6//////////////////////////////////  
 
 }
